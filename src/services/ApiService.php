dd9f74e9e9f271097a7a2e1de7c3e173dab9754d
mEsUsah <stanley@haxor.no>
2023-10-28 23:53:53 +0200

    fix: refactor userData intro ApiService, get badges

--- src/services/ApiService.php
+++ src/services/ApiService.php
@@ -1,31 +1,62 @@
 <?php
-/**
- * haxor plugin for Craft CMS 3.x
- *
- * Plugin for haxor.no
- *
- * @link      haxor.no
- * @copyright Copyright (c) 2023 Stanley Skarshaug
- */
 
- namespace mesusah\crafttryhackme\services;
+namespace mesusah\crafttryhackme\services;
 
 use mesusah\crafttryhackme\TryHackMe;
 
-use Craft;
 use craft\base\Component;
 
 class ApiService extends Component
 {
+    private $webEndpoint = "https://tryhackme.com";
+    private $assetsEndpoint = "https://assets.tryhackme.com";
+
     /**
      * Get the user rank from the TryHackMe API
      * 
      * @param string $username
      * @return array
      */
+
+     public function userData($username)
+     {
+         $data = $this->getUserRank($username);
+         
+         $badges = $this->getBadgesAll();
+         foreach ($badges as $badge) {
+             $data['badges'][$badge['name']] = [
+                 'title' => $badge['title'],
+                 'name' => $badge['name'],
+                 'description' => $badge['description'],
+                 'image' => $this->assetsEndpoint . $badge['image'],
+                 'earned' => false,
+             ];
+         }
+
+         return $data;
+     }
+
     public function getUserRank($username)
     {
-        $url = "https://tryhackme.com/api/discord/user/{$username}";
+        $url = "{$this->webEndpoint}/api/discord/user/{$username}";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+        $output = curl_exec($ch);
+        curl_close($ch);
+        $json = json_decode($output, true);
+        return $json;
+    }
+
+    /**
+     * Get all badges from the TryHackMe API
+     * 
+     * @param null
+     * @return array
+     */
+    public function getBadgesAll()
+    {
+        $url = "{$this->webEndpoint}/api/badges/get";
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
--- src/variables/TryHackMeVariable.php
+++ src/variables/TryHackMeVariable.php
@@ -37,6 +37,6 @@ class TryHackMeVariable
     public function userData()
     {
         $username = TryHackMe::getInstance()->getSettings()->username;
-        return TryHackMe::getInstance()->api->getUserRank($username);
+        return TryHackMe::getInstance()->api->userData($username);
     }
 }
