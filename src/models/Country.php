4f466fb2a7beccb0036b8765e6d9171b19a05efe
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2023-10-28 19:29:57 +0200

    fix(settings): fix typo

--- src/models/Country.php
+++ src/models/Country.php
@@ -4,11 +4,17 @@ namespace mesusah\crafttryhackme\models;
 
 use Craft;
 use craft\db\ActiveRecord;
+use mesusah\crafttryhackme\models\User;
 
 class Country extends ActiveRecord
 {
     public static function tableName()
     {
         return '{{%tryhackme_country}}';
+    }
+
+    public function users()
+    {
+        return $this->hasMany(User::class, ['country_id' => 'id']);
     }
 }
