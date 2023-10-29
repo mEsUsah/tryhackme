80562b487b1333bb6b5c6e7dff839fcfbd5a3a50
mEsUsah <stanley@haxor.no>
2023-10-29 15:57:22 +0100

    fix(user_score): add relation to user

--- src/models/UserScore.php
+++ src/models/UserScore.php
@@ -4,11 +4,17 @@ namespace mesusah\crafttryhackme\models;
 
 use Craft;
 use craft\db\ActiveRecord;
+use mesusah\crafttryhackme\models\User;
 
 class UserScore extends ActiveRecord
 {
     public static function tableName()
     {
         return '{{%tryhackme_user_score}}';
+    }
+
+    public function user()
+    {
+        return $this->hasOne(User::class, ['id' => 'user_id']);
     }
 }
